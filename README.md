aedaf4f70bbb39502fb71588b5f8d3abd6dbed1d
Bart Hendriks <bart.hendriks@ineos.com>
2023-06-22 14:11:03 +0200

    Updated script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# htaccess-manager" 
+# htaccess-manager
+
+htaccess manager is for securing directories on an apache web server.
+With this script you can create a realm (private zone), add,view and delete members.
+
+Before you create a secure folder, you must first register at least one member, otherwise nobody will be able to access the folder!
--- htaccess.php
+++ htaccess.php
@@ -1,20 +1,19 @@
 <?php
 session_start();
 ###########################################################################
-#                          BWL htaccess manager                           #
+#                      BartBandit htaccess manager                        #
 ###########################################################################
 #                                                                         #
-#                Copyright 2002 by BartsWeblabo                           #
-#     Contact: bart@bartsweblabo.com || http://www.bartsweblabo.com       #
-#               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~			  #  
-#                   Enjoy the script, its free				  #
+#                    https://github.com/BartBandit/                       #
+#               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~			          #  
+#                   Enjoy the script, its free				              #
 ###########################################################################
 # config
 #########
 $password_2_enter = "LetMeIn";          // the pasword to enter this script
 $pwfile = ".htpasswd";                  // the password file
 $CR_LF = "\r\n";                        // use "\r\n" ore "\n" for newline
-$mydomain = "iowa.ineosoxide.com"; 		// realm - found at: $mydomain
+$mydomain = "github.com/BartBandit/  "; 		// realm - found at: $mydomain
 ###################################
 # functions
 ############
@@ -26,7 +25,7 @@ function error_message($msg) {
 }
 
 function html_footer() {
-    echo "<p><a href=\"http://www.bartsweblabo.be\">2002 &copy; BartsWebLabo</a></p>";
+    echo "<p><a href=\"https://github.com/BartBandit/\">https://github.com/BartBandit/</a></p>";
     echo "</body>\n</html>";
 }
 
@@ -65,7 +64,7 @@ function test_input($data) {
 ?>
 <html>
     <head>
-        <title>BWL htaccess manager v1.0</title>
+        <title>BartBandit htaccess manager v2.0</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
         <style type="text/css">
             <!--
@@ -77,16 +76,16 @@ function test_input($data) {
     </head>
 
     <body bgcolor="#000000" text="#FFFFFF" leftmargin="100" link="#FFFFFF" vlink="#CCCCCC" alink="#999999">
-        <h2>BWL htaccess manager v1.0</h2>
+        <h2>BartBandit htaccess manager v2.0</h2>
         <?php
 ###################################
 # check password
 #################
         if ($password_2_enter != $password) {
             ?>
-            <p><b>Paswoord ingeven</b></p>
+            <p><b>Provide password</b></p>
             <form action="htaccess.php" method="post">
-                Paswoord: <input type="text" name="password"> <input type="submit" value="Inloggen">
+                Password: <input type="text" name="password"> <input type="submit" value="Log in">
             </form>
             <br>
             <br>
@@ -100,11 +99,11 @@ function test_input($data) {
 ###########
         ?>
         <a href="htaccess.php">Index</a> | 
-        <a href="htaccess.php?doe=create">Beveiligde map maken</a> | 
-        <a href="htaccess.php?doe=destroy">Beveiliging ongedaan maken</a> | 
-        <a href="htaccess.php?doe=new"> Nieuw lid</a> | 
-        <a href="htaccess.php?doe=del">Verwijder lid</a> | 
-        <a href="htaccess.php?doe=show">Toon Leden</a><br>
+        <a href="htaccess.php?doe=create">Create secure folder</a> | 
+        <a href="htaccess.php?doe=destroy">Undo protection</a> | 
+        <a href="htaccess.php?doe=new"> New member</a> | 
+        <a href="htaccess.php?doe=del">Remove member</a> | 
+        <a href="htaccess.php?doe=show">Show members</a><br>
         <br>
 
         <?php
@@ -113,20 +112,20 @@ function test_input($data) {
 ############
         if ($doe == "new") {
             ?>
-            <p><b>Nieuw lid aanmaken</b></p>
+            <p><b>Add member</b></p>
             <form action="htaccess.php" method="post">
-                Geef de gebruikersnaam:<br>
+                Enter the username:<br>
                 <input type="text" name="username">
                 <br>
                 <br>
-                Geef het wachtwoord tweemaal:<br>
+                Enter the password twice:<br>
                 <input type="password" name="pwd1">
                 <br>
                 <input type="password" name="pwd2">
                 <input type="hidden" name="saveuser" value="yes">
                 <br>
                 <br>
-                <input type="submit" name="submit" value="Toevoegen">
+                <input type="submit" name="submit" value="Add">
             </form>
             <?php
             html_footer();
@@ -134,35 +133,35 @@ function test_input($data) {
         }
         if ($_saveuser == "yes") {
             if ($_username == "" || $_pwd1 == "" || $_pwd2 == "") {
-                error_message("Ben je niets vergeten???!!!");
+                error_message("Did you forget anything???!!!");
             } else {
                 if (!file_exists($pwfile)) { // first time here, lets create the pwfile
                     if (!$fp = @fopen($pwfile, "w+"))
-                        die("Kan de file niet maken!");
+                        die("Cannot create the file!");
                     fclose($fp);
                 }
                 if (!($fp = fopen($pwfile, "r")))
-                    die("Kan $pwfile niet openen");
+                    die("Can not open " . $pwfile);
                 $file_contents = @fread($fp, filesize($pwfile));
                 fclose($fp);
                 $lines = explode($CR_LF, $file_contents);
                 foreach ($lines as $line) {
                     @list($member, $pass) = explode(':', $line);
                     if ($member == $_username)
-                        error_message("Deze naam komt reeds voor, kies een andere.");
+                        error_message("This name already exists, choose another one.");
                     if ($_pwd1 == $_pwd2) {
                         $passwd = $_pwd2;
                         $data = "$_username:$passwd$CR_LF";
                     } else
-                        error_message("Wachtwoorden komen niet overeen");
+                        error_message("Passwords do not match");
                 }
             }
             $fp = fopen($pwfile, "a+");
             if (!fwrite($fp, $data))
-                error_message("Er kon niet geschreven worden naar $pwfile. Is deze map 777 gechmod??!!");
+                error_message("Unable to write to $pwfile. Is this folder chmod 777 ??!!");
             fclose($fp);
-            echo '<p><b>Nieuw lid</b></p>';
-            echo "User $_username is aangemaakt met wachtwoord $_pwd2";
+            echo '<p><b>New member</b></p>';
+            echo "User $_username is created with password $_pwd2";
             html_footer();
             exit;
         }
@@ -171,13 +170,13 @@ function test_input($data) {
 ################
         if ($doe == "del") {
             ?>
-            <p><b>Verwijder lid</b></p>
+            <p><b>Remove member</b></p>
             <form method="post" action="htaccess.php">
-                naam:<input type="text" name="username" maxlength="15">
+                Name:<input type="text" name="username" maxlength="15">
                 <br>
                 <input type="hidden" name="deluser" value="yes">
                 <br>
-                <input type="submit" value="Verwijderen">
+                <input type="submit" value="Remove">
             </form>
             <?php
             html_footer();
@@ -199,13 +198,13 @@ function test_input($data) {
                     $new_pwfile .= "$member:$pass$CR_LF";
             }
             if ($count == 0)
-                error_message($_username . " staat niet in de lijst");
+                error_message($_username . " is not in the list.");
             $fp = fopen($pwfile, "w");
             fwrite($fp, $new_pwfile);
             fclose($fp);
             ?>
-            <p><b>Lid verwijderen</b></p>
-            <?php echo $_username ?> is verwijderd.<br>
+            <p><b>Remove member</b></p>
+            <?= $_username ?> is removed.<br>
             <?php
             html_footer();
             exit;
@@ -228,9 +227,9 @@ function test_input($data) {
                     $show_pwfile .= "$count : $member<br>\n";
                 }
             }if ($count == 1)
-                error_message("Er zijn geen leden");
+                error_message("There are no members");
             ?>
-            <p><b>Toon leden</b></p>
+            <p><b>Show members</b></p>
             <?php
             echo $show_pwfile;
             html_footer();
@@ -241,18 +240,18 @@ function test_input($data) {
 ########################
         if ($doe == "create") {
             if (!file_exists($pwfile))
-                error_message("Je moet eerst een lid aanmaken, anders kan je straks niet inloggen!");
+                error_message("You must first create a member, otherwise you will not be able to log in!");
             if (file_exists("./.htaccess"))
-                error_message("Deze map is reeds beveiligd");
+                error_message("This folder is already protected");
             ?>
-            <p><b>Beveiligde map aanmaken</b></p>
+            <p><b>Create secure folder</b></p>
             <form action="htaccess.php" method="post">
-                Naam van de realm(max 15 karakters)<br>
+                Name of the realm(max 15 characters)<br>
                 <input type="text" name="realmname" maxlenght="15">
                 <br>
                 <input type="hidden" name="createrealm" value="yes">
                 <br>
-                <input type="submit" value="Beveilig deze map">
+                <input type="submit" value="Protect this folder">
             </form>
 
             <?php
@@ -265,11 +264,11 @@ function test_input($data) {
             $htaccesscontent = "AuthType Basic\nAuthName \"" . $_realmname . " - found at: " . $mydomain . "\"\nAuthUserFile " . $path . $pwfile . "\nrequire valid-user";
             $fp = fopen("./.htaccess", "w+");
             if (!fwrite($fp, $htaccesscontent))
-                error_message(".htaccess kon niet gemaakt worden! Gelieve deze map naar 777 te chmoden!");
+                error_message(".htaccess could not be made! Please chmod this directory to 777!");
             fclose($fp);
-            echo "Deze map is met succes beveiligd,<br>\n";
-            echo "vergeet je login en wachtwoord niet, je zult het dadelijk nodig hebben<br>\n";
-            echo "hieronder staat de inhoud van .htacces:<br></br>\n";
+            echo "This folder has been successfully secured,<br>\n";
+            echo "don't forget your login and password, you will need it soon.<br>\n";
+            echo "Below is the content of .htacces:<br></br>\n";
             echo "<pre>$htaccesscontent</pre><br>\n";
             html_footer();
             exit;
@@ -279,47 +278,46 @@ function test_input($data) {
 ###############################
         if ($doe == "destroy") {
             ?>
-            <p><b>Beveiliging ongedaan maken</b></p>
-            Weet je zeker dat deze map terug toegangkelijk mag zijn voor iedereen?<br>
+            <p><b>Undo protection</b></p>
+            Are you sure that this folder should be accessible to everyone again?<br>
             <form action="htaccess.php" method="post">
                 <input type="hidden" name="destroyrealm" value="yes">
-                <input type="submit" value="Beveiliging ongedaan maken">
+                <input type="submit" value="Undo protection">
             </form>
             <?php
             html_footer();
             exit;
         }
         if ($_destroyrealm == "yes") {
-            echo "<p><b>Beveiliging ongedaan maken</b></p>\n";
+            echo "<p><b>Undo protection</b></p>\n";
             if (file_exists("./" . $pwfile)) {
                 if (!unlink("./" . $pwfile)) {
-                    echo "$pwfile kon niet verwijderd worden! Gelieve manueel met FTP te verwijderen.<br>\n";
+                    echo "$pwfile could not be deleted! Please remove manually with FTP.<br>\n";
                 } else {
-                    echo "$pwfile is verwijderd.<br>\n";
+                    echo "$pwfile is deleted.<br>\n";
                 }
             } else {
-                echo "$pwfile bestaat niet!!??<br>\n";
+                echo "$pwfile does not exist!!??<br>\n";
             }
             if (file_exists("./.htaccess")) {
                 if (!unlink("./.htaccess")) {
-                    echo ".htaccess kon niet verwijderd worden! Gelieve manueel met FTP te verwijderen.<br>\n";
+                    echo ".htaccess could not be deleted! Please remove manually with FTP.<br>\n";
                 } else {
-                    echo ".htaccess is verwijderd.<br>\n";
+                    echo ".htaccess is deleted.<br>\n";
                 }
             } else {
-                echo ".htaccess bestaat niet!!??<br>\n";
+                echo ".htaccess does not exist!!??<br>\n";
             }
             html_footer();
             exit;
         }
         ?>
-        <p><b>Welkom</b></p>
-        htaccess manager is voor het beveiligen van mappen op een apache webserver.<br>
-        Met dit script kan je een realm (private zone) aanmaken, leden toevoegen, leden 
-        verwijderen en leden nakijken.<br>
+        <p><b>Welcome</b></p>
+        htaccess manager is for securing directories on an apache web server. 
+		With this script you can create a realm (private zone), add,view and delete members.<br>
         <br>
-        Voordat je een beveiligde map aanmaakt moet je eerst minstens &eacute;&eacute;n 
-        lid inschrijven, anders kan je zelf niet meer in de map.<br>
+        Before you create a secure folder, you must first register at least one member, 
+		otherwise nobody will be able to access the folder!<br>
         <br>
         <br>
         <br>
